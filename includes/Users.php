485ce226899d021a8c7085cec47f5b3a86670561
AlexTests123 <joetoxic97@gmail.com>
2021-06-24 15:42:57 +0300

    INSERT QUERY

--- includes/Users.php
+++ includes/Users.php
@@ -15,14 +15,14 @@ class Users extends DBconnection
 
         if(isset($_POST['submit'])){
 
-            echo $this->firstName = $_POST['firstName'];
-            echo $this->lastName = $_POST['lastName'];
-            echo $this->username = $_POST['username'];
-            echo  $this->email = $_POST['email'];
-            echo  $this->password = $_POST['password'];
+            $firstName = $this->firstName = $_POST['firstName'];
+            $lastName = $this->lastName = $_POST['lastName'];
+            $username = $this->username = $_POST['username'];
+            $email =  $this->email = $_POST['email'];
+            $password =  $this->password = $_POST['password'];
 
-            $query = "INSERT INTO users ('first_name','last_name','email','password')";
-            $query .= "VALUES ('$this->firstName', '$this->lastName','$this->username', '$this->email', '$this->password')";
+            $query = "INSERT INTO users (first_name,last_name,username,email,password) 
+              VALUES ('$firstName', '$lastName','$username', '$email', '$password') ";
 
 
             if(mysqli_query($conn,$query)){
